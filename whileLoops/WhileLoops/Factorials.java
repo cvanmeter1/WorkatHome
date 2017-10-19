476836dc07f3a5f855a57125b159ee00b1a27067
cvanmeter1 <c.vanmeter@bssdstd.net>
2017-10-19 14:12:25 -0500

    work for home
    work for home

---
+++ whileLoops/WhileLoops/Factorials.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Factorials here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Factorials
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        int count = 1;
+        int product = 0;
+        System.out.println("Enter a non-negative integer");
+    }
+}
